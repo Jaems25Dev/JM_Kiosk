13444351b3c0ed7053ded212a90637a52ee7ea2e
Junho Jeon <junhjeon@c1r1s5.42seoul.kr>
2025-01-13 19:09:19 +0900

    JM-19/feat: modify & add mockApi for product
    1.
      product 컴포넌트를 위한 mockApi를 만들었습니다.
    2.
      Category mockApi 를 product에대응할 수 있게 수정했습니다.

--- src/app/mockApi/getCategory/route.ts
+++ src/app/mockApi/getCategory/route.ts
@@ -1,6 +1,6 @@
 // eslint-disable-next-line import/prefer-default-export
 export async function GET() {
-  return new Response(JSON.stringify({ categorys: ['음료', '음식'] }), {
+  return new Response(JSON.stringify({ categorys: ['breakfast', 'lunch', 'dinner'] }), {
     status: 200,
     headers: { 'Content-Type': 'application/json' },
   });
