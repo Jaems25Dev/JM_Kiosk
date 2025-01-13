f4d0f8863a74cd5c62c79564d9306a2cca0e5930
Junho Jeon <junhjeon@c1r1s5.42seoul.kr>
2025-01-13 19:11:35 +0900

    JM-19/remove: delete categoryContainer
    1.
      Category를 받아오는 작업이 상위 컴포넌트로 이동하여 Container 를
      삭제했습니다.

--- src/component/category/categoryContainer.tsx
+++
@@ -1,7 +0,0 @@
-import CategorySlide from './categorySlide';
-
-export default async function CategoryContainer() {
-  const { categorys } = await (await fetch('http://localhost:3000/mockApi/getCategory')).json();
-
-  return <CategorySlide categorys={categorys} />;
-}
